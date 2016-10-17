fcb65e356ee2751eb32d27c5596ed6c55d42f690
Mustafa <suvemustafa@gmail.com>
2016-10-17 22:20:34 +0300

    HomePage Mustafa Süve Button added mustafa.html added layout.html added homepage updated

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
+@app.route('/mustafa')
+def mustafa():
+    return render_template('mustafa.html')
 
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
